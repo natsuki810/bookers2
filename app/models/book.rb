6b92c3bcf0f91cf97901e2fd5d8f5992697bbb37
EC2 Default User <ec2-user@ip-172-31-15-97.ap-northeast-1.compute.internal>
2023-11-14 12:56:57 +0000

    [finish]-sign_up

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  blongs_to :user
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,4 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-const webpack = require('webpack')
-enviroment.plugins.prepund(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: 'popper.is'
-  })
-  )
+
